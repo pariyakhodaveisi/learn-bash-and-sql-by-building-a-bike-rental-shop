418f59b52e007d14e2bf26765806f7c87136e4e8
Pariya Khodaveisi <paria.khodaveissi@gmail.com>
2024-03-26 09:02:27 +0000

    Save progress: 1680.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -139,7 +139,9 @@ RETURN_MENU() {
         # send to main menu
         MAIN_MENU "You do not have that bike rented."
         else 
-          
+          #update date_returned
+          #set bike availability to true
+          #send to main menu
       fi
     fi
    fi
